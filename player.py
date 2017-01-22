0c9f10bb4c59804cce3d76e849be034b47c4455c
Joshua Snider <jsnider@mtu.edu>
2017-01-22 09:54:10 -0500

    Starting over

--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-"""To Do:
-Work on decide() (it doesn't look functional at all)
+"""
+The player has a purse, an ante, and a hand
 
+(Plus a name, UID, and some state flags)
 """
 class Player:
   playerCnt = 0
 
   def __init__(self, name):
+    self.allIn = False
     self.name = name
     self.hand = []
     self.purse = 100
@@ -14,43 +16,10 @@ class Player:
     self.check = False
     self.fold = False
     self.dec = False
+    self.uniq = Player.playerCnt
     Player.playerCnt += 1
 
   def showHand(self):
-    data = '\n'+self.name+' is holding '+' '.join(self.hand)
-    return data
-
-  def decide(self):
-    dif = lastRaise - self.ante
-    if self.fold == True:
-      return 0
-    if dif == 0:
-      while k not in ('c', 'r', 'f'):
-        k = raw_input("[c] to check, [r] to raise, or [f] to fold: ")
-      if k == 'c':
-        print self.name+' checks'
-        self.check = True
-      elif k == 'r':
-        j = int(raw_input("How much? "))
-        print self.name+' raises $'+str(j)
-        self.rais(j)
-      elif k == 'f':
-        print self.name+' folds'
-        for i in range(2):
-          self.hand.pop()
-        self.fold = True
-    else:
-      while k not in ('c', 'r', 'f'):
-        k = raw_input("[c] to call $"+str(dif)+", [r] to raise, or [f] to fold: ")
-      if k == 'c':
-        print self.name+' calls $'+str(dif)
-        self.rais(dif)
-      elif k == 'r':
-        j = int(raw_input("How much? "))
-        print self.name+' raises $'+str(j)
-        self.rais(j)
-      elif k == 'f':
-        print self.name+' folds'
-        for i in range(2):
-          self.hand.pop()
-          self.fold = True
+    print('\n'+self.name+' is holding '+' '.join(self.hand))
+    print('\nTheir purse is currently: $'+str(self.purse))
+    print('Their ante is currently: $'+str(self.ante)+'\n')
