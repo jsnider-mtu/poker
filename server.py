2ca979edbae317bbace4ab749c3b9274f3bc40f1
jsnider-mtu <jsnider@mtu.edu>
2017-10-13 18:40:24 -0400

    Moved pitch back out of the server.py file and grabbed a barebones server made of one socket and threads for multiple clients, will probably move away from that basic model though

--- server.py
+++ server.py
@@ -1,167 +1,67 @@
-#!/usr/bin/python
-"""Pitch is the name I'm giving to the table structure
-desired in server.py
-
-TODO:
-* call and rais need to ensure purse doesn't go negative
-* finishHand requires a way of determining a winner
+#!/usr/bin/python3
 """
-import random
-from collections import deque
+This is originally from "Programming Python" 4th Ed. by Mark Lutz,
+published by O'Reilly
 
-class Pitch:
-    def __init__(self):
-        self.watchers = []
-        self.players = deque()
-        self.deck = ['AH','2H','3H','4H','5H','6H','7H','8H','9H','TH','JH',\
-                     'QH','KH','AD','2D','3D','4D','5D','6D','7D','8D','9D',\
-                     'TD','JD','QD','KD','AC','2C','3C','4C','5C','6C','7C',\
-                     '8C','9C','TC','JC','QC','KC','AS','2S','3S','4S','5S',\
-                     '6S','7S','8S','9S','TS','JS','QS','KS']
-        self.table = []
-        self.pot = 0
-        self.lastRaise = 0
+So how do I want the server<->client conversation to look?
 
-    def bettingRound(self):
-        if self.lastRaise == 10:
-            turn = 2
-        else:
-            turn = 0
-        while self.players[turn].dec == False or (self.players[turn].ante < \
-              self.lastRaise or (self.lastRaise == 0 and \
-              self.players[turn].check == False)):
-            if self.players[turn].fold == True:
-                if turn < len(self.players)-1:
-                    turn += 1
-                else:
-                    turn = 0
-                continue
-            print(self.players[turn].name+'\'s turn')
-            self.showPitch()
-            self.decide(self.players[turn])
-            self.players[turn].dec = True
-            if turn < len(self.players)-1:
-                turn += 1
-            else:
-                turn = 0
-        for play in self.players:
-            self.pot += play.ante
-            play.ante = 0
-            play.check = play.dec = False
-        self.lastRaise = 0
-        return 'pot is at $'+str(self.pot)
+Client talks first, server responds with HTTP 200 and new state
+(actually all clients need new state after an action taken by any
+player), or HTTP 500 in the case of an illegal move (shouldn't
+happen if using the client in this repo, I hope anyways), or HTTP 400
+in the case of gobbledy-gook. You know what, fuck it, let's define as
+many as possible:
 
-    def call(self, amount, player):
-        player.purse -= amount
-        player.ante += amount
+200 - OK (data is new state of the pitch)
+201 - Created (new player created, new table created, etc)
+302 - If HTTP move them to HTTPS (This will come later)
+400 - Bad Request (Gobbledy-gook as far as I am concerned)
+401 - Unauthorized (Not yet signed-in/registered)
+403 - Forbidden (Because Fuck You!, that's why)
+404 - Not Found
+405 - Method Not Allowed (Will have to include an Allow header)
+406 - Not Acceptable (Response entity characteristics mismatch)
+408 - Request Timeout (Don't think I'll need this one)          <-#
+409 - Conflict (Hopefully won't need to use this one either)
+410 - Gone (Table was destroyed somehow)
+505 - HTTP Version Not Supported (Let's only allow HTTP/1.1)
+"""
+import socket
+import threading
 
-    def deal(self):
-        for i in range(2):
-            for play in self.players:
-                play.hand.append(self.deck.pop())
+bind_ip = "0.0.0.0"
+bind_port = 9999
 
-    def decide(self,player):
-        try:
-            player.showHand()
-            if player.fold == True or player.allIn == True:
-                return 0
-            dif = self.lastRaise - player.ante
-            if dif >= player.purse:
-                player.allIn = True
-            k = 'a'
-            if dif == 0:
-                while k not in ('c', 'r', 'f'):
-                    k = input("[c] to check, [r] to raise, or [f] to fold: ")
-                if k == 'c':
-                    print(player.name+' checks')
-                    player.check = True
-                elif k == 'r':
-                    j = int(input("How much? "))
-                    self.rais(j,player)
-                    print(player.name+' raises $'+str(j))
-                elif k == 'f':
-                    print(player.name+' folds')
-                    for i in range(2):
-                        player.hand.pop()
-                    player.fold = True
-            else:
-                while k not in ('c', 'r', 'f'):
-                    if player.allIn:
-                        k = input("[c] to go ALL IN with $"+str(player.purse)+", or [f] to fold: ")
-                        while k not in ('c', 'f'):
-                            k = input("[c] to go ALL IN with $"+str(player.purse)+", or [f] to fold: ")
-                    else:
-                        k = input("[c] to call $"+str(dif)+", [r] to raise, or [f] to fold: ")
-                if k == 'c':
-                    self.call(dif,player)
-                    print(player.name+' calls $'+str(dif))
-                elif k == 'r':
-                    j = int(input("How much? "))
-                    self.call(dif,player)
-                    self.rais(j,player)
-                    print(player.name+' raises $'+str(j))
-                elif k == 'f':
-                    print(player.name+' folds')
-                    for i in range(2):
-                        player.hand.pop()
-                    player.fold = True
-        except NoFundsError:
-            self.decide(player)
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-    def finishHand(self):
-        winner = 0
-        self.players[winner].purse += self.pot
-        self.pot = 0
-        for play in self.players:
-            play.showHand()
-            play.fold = False
-            play.hand = []
-        return 'Hand done'
+server.bind((bind_ip,bind_port))
 
-    def flop(self):
-        self.deck.pop()
-        for i in range(3):
-            self.table.append(self.deck.pop())
+server.listen(100)
 
-    def rais(self, amount, player):
-        if player.purse - amount < 0:
-            print("You don't have the funds\n")
-            raise NoFundsError("You don't have the funds")
-        else:
-            player.purse -= amount
-            player.ante += amount
-            self.lastRaise = player.ante
+print("[*] Listening on %s:%d" % (bind_ip,bind_port))
 
-    def showPitch(self):
-        print('\nCurrently Playing:')
-        for play in self.players:
-            print(' '+play.name+' ',end='')
-        print('\n\nCurrently Watching:')
-        for watch in self.watchers:
-            print(' '+watch.name+' ',end='')
-        print('\n# of Cards Remaining in Deck: '+str(len(self.deck)),end='')
-        print(' | Current Pot: $'+str(self.pot)+' | Last Raise: $'+\
-              str(self.lastRaise))
+# this is our client-handling thread
+def handle_client(client_socket):
+  
+  # print out what the client sends
+  request = client_socket.recv(1024)
+  
+  print("[*] Received: %s" % request)
 
-    def showTable(self):
-        print('\nOn the table: '+' '.join(self.table))
-        for i in self.players:
-            i.showHand()
-
-    def shuffleDeck(self):
-        for i in range(random.randint(1,6)):
-            random.shuffle(self.deck)
-
-    def turnRiver(self):
-        self.deck.pop()
-        self.table.append(self.deck.pop())
-
-class NoFundsError(Exception):
-    pass
-
-class Server:
-  def __init__(self, hostname=localhost):
-    hostname = self.hostname
-
-if __name__ == '__main__':
-  s = Server()
+  # Parse the request
+  
+  
+  # send back a packet
+  client_socket.send("ACK!")
+  
+  client_socket.close()
+  
+while True:
+  
+  client,addr = server.accept()
+  
+  print("[*] Accepted connection from: %s:%d" % (addr[0],addr[1]))
+  
+  # spin up our client thread to handle incoming data
+  client_handler = threading.Thread(target=handle_client,args=(client,))
+  client_handler.start()
