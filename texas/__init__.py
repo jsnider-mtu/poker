57e19de5ad4e83692ecea073659f30f12cd8c88f
Joshua Snider <jsnider@mtu.edu>
2022-12-22 21:14:39 -0500

    Test imports in __init__.py

--- texas/__init__.py
+++ texas/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from deck import *
-from player import *
-from game import *
+from texas.deck import *
+from texas.player import *
+from texas.game import *
 
 __version__ = '0.0.1'
