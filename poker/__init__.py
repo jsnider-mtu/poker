e59212bfbe33d7525a9388b706d56f5a02d9a0ee
Joshua Snider <jsnider@mtu.edu>
2022-12-25 21:22:39 -0500

    Fix __init__.py

--- poker/__init__.py
+++ poker/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from texas.deck import *
-from texas.player import *
-from texas.game import *
+from poker.deck import *
+from poker.player import *
+from poker.game import *
 
 __version__ = '0.0.1'
